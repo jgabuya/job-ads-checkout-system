489024f7f6584018da5674ceb792faca4c6646d8
Jirffey Gabuya <boyhimas@gmail.com>
2017-06-14 16:49:34 +0800

    Added routes

--- app/Http/Controllers/Api/AdController.php
+++ app/Http/Controllers/Api/AdController.php
@@ -19,16 +19,6 @@ class AdController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -46,17 +36,6 @@ class AdController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Ad $ad)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Ad  $ad
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Ad $ad)
     {
         //
     }
