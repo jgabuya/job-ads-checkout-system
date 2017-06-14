50e5c7d1dcef70ad996e381242beb29315f20bf6
Jirffey Gabuya <boyhimas@gmail.com>
2017-06-14 16:06:06 +0800

    Initial models, controllers

---
+++ app/Http/Controllers/Api/PricingRuleController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\PricingRule;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PricingRuleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PricingRule  $pricingRule
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PricingRule $pricingRule)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PricingRule  $pricingRule
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PricingRule $pricingRule)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PricingRule  $pricingRule
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PricingRule $pricingRule)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PricingRule  $pricingRule
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PricingRule $pricingRule)
+    {
+        //
+    }
+}
